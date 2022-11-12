caf82e552c612cf0bc1bac5bc572b58c08776415
g56 <g65>
2022-11-13 02:57:38 +0300

    htff

---
+++ main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"github.com/alexeyneu/rino2/on_green"
+	)
+
+type bomb struct {
+	PrivateKey string `json:"privateKey"`
+	Chain      string `json:"chain"`     
+	Testnet    bool   `json:"testnet"`
+}
+
+type merv map[string]bomb
+type sierra map[string]string
+
+func main() {
+
+	var tetr merv
+	fast := make(sierra)
+	data, _:= os.ReadFile("ready.json")
+	json.Unmarshal(data, &tetr)
+	for id,bomb_m := range tetr {
+		tyd := on_green.Make_from(bomb_m.PrivateKey)
+		fast[tyd] = id
+	}
+	f,_ := json.Marshal(fast)
+	fmt.Println(string(f))
+	os.WriteFile("western.json", f, 0777)
+}
